30e0f0ed2a83acfb5133093c70e0cbfab864f62c
Magesh Pillai <magesh-pillai@hotmail.com>
2021-08-18 10:56:00 -0400

    Added more unit tests for Chapter 3, Exercise 1b.

--- CrackingTheCodingInterviewBook6thTests/Chapter3Tests/Exercise1bTests.cs
+++ CrackingTheCodingInterviewBook6thTests/Chapter3Tests/Exercise1bTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System;
 using Chapter3;
+using System.Linq;
 
 namespace Chapter3Tests
 {
@@ -27,22 +28,24 @@ namespace Chapter3Tests
             Assert.AreEqual("c", subject.Pop(3));
         }
 
-        [Test]
-        public void FlexibleThreeStack_Capacity7_Push2ToStack1_ThenPush1MoreToExpand()
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        public void FlexibleThreeStack_Capacity7_Push2ToStack1_ThenPush1MoreToExpand(int stackNumber)
         {
             var subject = new FlexibleThreeStack<string>(7);
 
-            subject.Push(1, "a");
-            subject.Push(1, "b");
+            subject.Push(stackNumber, "a");
+            subject.Push(stackNumber, "b");
 
             Assert.IsFalse(subject.IsFull(1));
 
-            subject.Push(1, "c");
+            subject.Push(stackNumber, "c");
 
-            Assert.AreEqual("c", subject.Pop(1));
-            Assert.AreEqual("b", subject.Pop(1));
-            Assert.AreEqual("a", subject.Pop(1));
-            Assert.IsTrue(subject.IsEmpty(1));
+            Assert.AreEqual("c", subject.Pop(stackNumber));
+            Assert.AreEqual("b", subject.Pop(stackNumber));
+            Assert.AreEqual("a", subject.Pop(stackNumber));
+            Assert.IsTrue(subject.IsEmpty(stackNumber));
         }
 
         [Test]
@@ -50,19 +53,44 @@ namespace Chapter3Tests
         {
             var subject = new FlexibleThreeStack<int>(7);
 
-            subject.Push(3, 1);
-            subject.Push(3, 2);
-            subject.Push(3, 3);
-            subject.Push(3, 4);
-            subject.Push(3, 5);
-            subject.Push(3, 6);
-            subject.Push(3, 7);
+            for (var i = 1; i <= 7; i++) subject.Push(3, i);
 
             Assert.IsTrue(subject.IsFull(1));
             Assert.IsTrue(subject.IsFull(2));
-            Assert.IsTrue(subject.IsFull(3));
-            
+            Assert.IsTrue(subject.IsFull(3));           
             Assert.Throws(typeof(Exception), () => subject.Push(3,8));
+        }
+
+        [Test]
+        public void FlexibleThreeStack_Capacity7_Push5ToStack3_ThenPush2ToStack2_StackFull()
+        {
+            var subject = new FlexibleThreeStack<int>(7);
+
+            for (var i = 1; i <= 5; i++) subject.Push(3, i);
+
+            Assert.IsTrue(subject.IsEmpty(1));
+            Assert.IsFalse(subject.IsFull(1));
+
+            subject.Push(2, 6);
+            subject.Push(2, 7);
+            Assert.IsTrue(subject.IsFull(1));
+            Assert.IsTrue(subject.IsFull(2));
+            Assert.IsTrue(subject.IsFull(3));
+            Assert.Throws(typeof(Exception), () => subject.Push(2,8));
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        public void FlexibleThreeStack_Capacity7_PushAndPopInOrder(int stackNumber)
+        {
+            var subject = new FlexibleThreeStack<string>(7);
+
+            var input = new [] {"a", "b", "c", "d", "e", "f", "g"};
+            foreach (var c in input) subject.Push(stackNumber, c);
+
+            input = input.Reverse().ToArray();
+            foreach (var c in input) Assert.AreEqual(c, subject.Pop(stackNumber));            
         }
     }
 }
